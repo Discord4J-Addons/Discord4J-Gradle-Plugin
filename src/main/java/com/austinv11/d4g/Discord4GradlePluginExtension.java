da312a3971e56377241973be51b90be8e524b1fc
austinv11 <austinv11@gmail.com>
2016-06-21 15:45:51 -0400

    Cleans up some code

--- src/main/java/com/austinv11/d4g/Discord4GradlePluginExtension.java
+++ src/main/java/com/austinv11/d4g/Discord4GradlePluginExtension.java
@@ -6,45 +6,9 @@ package com.austinv11.d4g;
 public class Discord4GradlePluginExtension {
 	
 	/**
-	 * If true, the plugin will automatically find IModule implementations, if false they must be provided by {@link #modules}.
-	 */
-	private boolean scanForModules = true;
-	/**
-	 * If {@link #scanForModules} is false, it loads these modules.
-	 */
-	private String[] modules = new String[0];
-	/**
-	 * The Discord4J version to use;
-	 */
-	private String version = "dev-SNAPSHOT";
-	/**
 	 * The bot API token to use to log into discord with.
 	 */
 	private String apiKey;
-	
-	public boolean isScanForModules() {
-		return scanForModules;
-	}
-	
-	public void setScanForModules(boolean scanForModules) {
-		this.scanForModules = scanForModules;
-	}
-	
-	public String[] getModules() {
-		return modules;
-	}
-	
-	public void setModules(String[] modules) {
-		this.modules = modules;
-	}
-	
-	public String getVersion() {
-		return version;
-	}
-	
-	public void setVersion(String version) {
-		this.version = version;
-	}
 	
 	public String getApiKey() {
 		return apiKey;
--- src/main/java/com/austinv11/d4g/tasks/ModuleTask.java
+++ src/main/java/com/austinv11/d4g/tasks/ModuleTask.java
@@ -26,25 +26,36 @@ public class ModuleTask extends JavaExec {
 		if (extension == null)
 			extension = new Discord4GradlePluginExtension();
 		
+		//This module depends on the jar task, use it for output info
 		Jar jar = (Jar) getProject().getTasks().findByName("jar");
+		
+		//Sets account info for d4j
 		setArgs(Collections.singletonList(extension.getApiKey()));
 //		setJvmArgs(Collections.singletonList("-Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG"));
 		setMain("sx.blah.discord.Discord4J");
+		
 		List<File> deps = new ArrayList<>();
 		File discordDir = new File("discord4j");
 		File modulesDir = new File("modules");
-		discordDir.mkdir();
-		modulesDir.mkdir();
-		getProject().getConfigurations().getByName("runtime").forEach(file -> {
+		if ((!discordDir.mkdir() && !discordDir.exists() && !discordDir.isDirectory()) 
+				|| (!modulesDir.mkdir() && !modulesDir.exists() && !modulesDir.isDirectory()))
+			throw new RuntimeException("Unable to generate required directories for running the modules");
+		
+		getProject().getConfigurations().getByName("runtime").forEach(file -> { //Moves dependencies to a "discord4j" folder
 			File newFile = new File(discordDir, file.getName());
-			file.renameTo(newFile);
+			if (!file.renameTo(newFile))
+				throw new RuntimeException("Unable to move "+file.getPath()+" to "+newFile.getPath());
+			
 			deps.add(newFile);
 		});
-		setClasspath(getProject().files(deps.toArray()));
+		setClasspath(getProject().files(deps.toArray())); //Sets the classpath to include the dependencies
 		
-		jar.getOutputs().getFiles().forEach(file -> {
-			if (file.exists() && !file.isDirectory() && file.getAbsolutePath().endsWith(".jar"))
-				file.renameTo(new File(modulesDir, file.getName()));
+		jar.getOutputs().getFiles().forEach(file -> { //Moves compiled binaries to the "modules" folder
+			if (file.exists() && !file.isDirectory() && file.getAbsolutePath().endsWith(".jar")) {
+				File newFile = new File(modulesDir, file.getName());
+				if (!file.renameTo(newFile))
+					throw new RuntimeException("Unable to move "+file.getPath()+" to "+newFile.getPath());
+			}
 		});
 		
 		super.exec();
